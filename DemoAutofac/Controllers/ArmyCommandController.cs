810442ef396b26cd474e7838e0980a6ddb1f5063
Hank <aken1215@hotmail.com>
2016-08-14 13:34:38 +0800

    新增騎兵強化版實作

---
+++ DemoAutofac/Controllers/ArmyCommandController.cs
@@ -0,0 +1,53 @@
+﻿using DemoAutofac.Services.Archer;
+using DemoAutofac.Services.Cavalry;
+using DemoAutofac.Services.Infantry;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace DemoAutofac.Controllers
+{
+    public class ArmyCommandController : ApiController
+    {
+        private IArcher ArcherService;
+        private ICavalry CavalryService;
+        private IInfantry InfantryService;
+
+        public ArmyCommandController(IInfantry infantryService, ICavalry cavalryService, IArcher archerService)
+        {
+            this.InfantryService = infantryService;
+            this.CavalryService = cavalryService;
+            this.ArcherService = archerService;
+        }
+
+        [HttpGet]
+        [ResponseType(typeof(string))]
+        public string SendOrder(ArmyType Id)
+        {
+            var result = "";
+            switch (Id)
+            {
+                case ArmyType.Infantry:
+                    result = this.InfantryService.Guard();
+                    break;
+                case ArmyType.Cavalry:
+                    result = this.CavalryService.Charge();
+                    break;
+                case ArmyType.Archer:
+                    result = this.ArcherService.MultiShot();
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+
+public enum ArmyType
+{
+    Infantry = 0,
+
+    Cavalry = 1,
+
+    Archer=2
+}
+}
---
+++ DemoAutofac/Services/Cavalry/PowerCavalry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoAutofac.Services.Cavalry
+{
+public class PowerCavalry : ICavalry
+{
+    public string Charge()
+    {
+        return "衝鋒後進行踐踏";
+    }
+}
+}
