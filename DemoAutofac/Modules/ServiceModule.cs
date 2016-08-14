e5a764abade65866fe885d71a6fcdd68eca6a012
Hank <aken1215@hotmail.com>
2016-08-14 13:33:49 +0800

    設定Autofac

---
+++ DemoAutofac/Modules/ServiceModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using DemoAutofac.Services.Archer;
+using DemoAutofac.Services.Cavalry;
+using DemoAutofac.Services.Infantry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoAutofac.Modules
+{
+public class ServiceModule: Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<Archer>().As<IArcher>();
+        builder.RegisterType<Infantry>().As<IInfantry>();
+        builder.RegisterType<Cavalry>().As<ICavalry>();
+    }
+}
+}
--- DemoAutofac/Startup.cs
+++ DemoAutofac/Startup.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Owin;
 using Owin;
+using Autofac;
+using System.Reflection;
+using System.Web.Http;
+using Autofac.Integration.WebApi;
+using Autofac.Configuration;
 
 [assembly: OwinStartup(typeof(DemoAutofac.Startup))]
 
@@ -13,6 +18,20 @@ namespace DemoAutofac
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            ConfigureAutofac(app);
+        }
+
+        private void ConfigureAutofac(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // Register individual components
+            builder.RegisterModule(new ConfigurationSettingsReader("autofac"));
+            var container = builder.Build();
+
+            var config = GlobalConfiguration.Configuration;
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
         }
     }
 }
